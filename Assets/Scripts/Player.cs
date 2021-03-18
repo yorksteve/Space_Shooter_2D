e1f72b1f7f70961cfbb7cae50913081a3cf92907
Steve <yorkspy@gmail.com>
2021-03-17 15:52:28 -1000

    Clamped player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 3.5f;
 
 
+
     void Start()
     {
-        transform.position = new Vector3(0f, 0f, 0f);
+        transform.position = new Vector3(0f, -2f, 0f);
     }
 
     void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.5f, 0f), transform.position.z);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, transform.position.z);
+        }
     }
 }
